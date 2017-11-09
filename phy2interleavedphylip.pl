96f0f1b6e583b2d864f991d924aee865f22fbf81
Steven Mussmann <smussmann@gmail.com>
2017-11-09 08:29:07 -0600

    added converter from phylip to interleaved phylip

--- phy2interleavedphylip.pl
+++ phy2interleavedphylip.pl
@@ -3,6 +3,7 @@
 use warnings;
 use strict;
 use Getopt::Std;
+use Data::Dumper;
 
 # kill program and print help if no command line arguments were given
 if( scalar( @ARGV ) == 0 ){
@@ -37,8 +38,8 @@ while( my $line = <PHY> ){
 
 close PHY;
 
-# get rid of phylip header
-shift( @lines );
+# capture phylip header
+my $header = shift( @lines );
 
 foreach my $line( @lines ){
 	my @temp = split( /\s+/, $line );
@@ -46,12 +47,53 @@ foreach my $line( @lines ){
 	push( @seqs, $temp[1] );
 }
 
+#determine how many rounds of printing need to occur
+my @headparts = split( /\s+/, $header);
+my $prints = int($headparts[1]/50)-1;
+my $remainder = $headparts[1]%50;
+#print $prints, "\n";
+#print $remainder, "\n";
+
+# print first section of file
 open( OUT, '>', $out ) or die "Can't open $out: $!\n\n";
 
-for(my $i=0; $i<@names; $i++){
-	print OUT ">$names[$i]\n";
-	print OUT "$seqs[$i]\n";
+print OUT $header, "\n";
+
+if( $prints == 0 ){
+	for( my $i=0; $i<@names; $i++ ){
+		print OUT $names[$i];
+		print OUT "\t";
+		print OUT $seqs[$i], "\n";
+	}
+}else{
+	for(my $i=0; $i<@names; $i++){
+		print OUT $names[$i];
+		print OUT "\t";
+		my $sub = substr( $seqs[$i], 0, 50, "" );
+		print OUT $sub, "\n";
+	}
 }
+
+#print middle section of file
+if( $prints > 0 ){
+	for( my $i=0; $i<$prints; $i++ ){
+		print OUT "\n";
+		for( my $j=0; $j<@seqs; $j++ ){
+			my $sub = substr( $seqs[$j], 0, 50, "" );
+			print OUT $sub, "\n";
+		}
+	}
+}
+
+#print final section of file
+if( $remainder > 0 && $prints != 0 ){
+	print OUT "\n";
+	foreach my $line( @seqs ){
+		print OUT $line, "\n";
+	}
+}
+
+#print Dumper(\@seqs);
 
 close OUT;
 
@@ -64,7 +106,7 @@ exit;
 # subroutine to print help
 
 sub help{
-	print "\nphy2fasta.pl is a perl script developed by Steven Michael Mussmann\n\n";
+	print "\nphy2interleavedphylip.pl is a perl script developed by Steven Michael Mussmann\n\n";
 	print "To report bugs send an email to mussmann\@email.uark.edu\n";
 	print "When submitting bugs please include all input files, options used for the program, and all error messages that were printed to the screen\n\n";
 	print "Program Options:\n";
@@ -72,7 +114,7 @@ sub help{
 	print "\t-h:\tUse this flag to display this help message.\n";
 	print "\t\tThe program will die after the help message is displayed.\n\n";
 	print "\t-o:\tUse this flag to specify the output file name.\n";
-	print "\t\tIf no name is provided, the file extension \".fasta\" will be appended to the input file name.\n\n";
+	print "\t\tIf no name is provided, the file extension \".phy\" will be appended to the input file name.\n\n";
 	print "\t-p:\tUse this flag to specify the name of a fasta file.\n\n";
 }
 
@@ -85,7 +127,7 @@ sub parsecom{
 
 	# set default values for command line arguments
 	my $file = $opts{p} || die "No input file specified.\n\n"; #used to specify input phylip file name.
-	my $out = $opts{o} || "$file.fasta"  ; #used to specify output file name.  If no name is provided, the file extension ".fasta" will be appended to the input file name
+	my $out = $opts{o} || "$file.phy"  ; #used to specify output file name.  If no name is provided, the file extension ".phy" will be appended to the input file name
 
 	return( $file, $out );
 }
